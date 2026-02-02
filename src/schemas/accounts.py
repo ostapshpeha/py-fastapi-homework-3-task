49eaab30a31714572ef26687b8370fe037c7fd88
Ostap Shp <stark171705@gmail.com>
2026-02-02 16:48:12 +0200

    completed

--- src/schemas/accounts.py
+++ src/schemas/accounts.py
@@ -2,7 +2,19 @@ import re
 
 from pydantic import BaseModel, EmailStr, field_validator, ConfigDict
 
-from database import accounts_validators
+
+def validate_password_complexity(v: str) -> str:
+    if len(v) < 8:
+        raise ValueError("Password must contain at least 8 characters.")
+    if not any(char.isupper() for char in v):
+        raise ValueError("Password must contain at least one uppercase letter.")
+    if not any(char.isdigit() for char in v):
+        raise ValueError("Password must contain at least one digit.")
+    if not any(char.islower() for char in v):
+        raise ValueError("Password must contain at least one lower letter.")
+    if not re.search(r"[@$!%*?#&]", v):
+        raise ValueError("Password must contain at least one special character: @, $, !, %, *, ?, #, &.")
+    return v
 
 
 class UserBase(BaseModel):
@@ -15,17 +27,7 @@ class UserRegistrationRequestSchema(UserBase):
     @field_validator("password")
     @classmethod
     def validate_password(cls, v: str) -> str:
-        if len(v) < 8:
-            raise ValueError("Password must contain at least 8 characters.")
-        if not any(char.isupper() for char in v):
-            raise ValueError("Password must contain at least one uppercase letter.")
-        if not any(char.isdigit() for char in v):
-            raise ValueError("Password must contain at least one digit.")
-        if not any(char.islower() for char in v):
-            raise ValueError("Password must contain at least one lower letter.")
-        if not re.search(r"[@$!%*?#&]", v):
-            raise ValueError("Password must contain at least one special character: @, $, !, %, *, ?, #, &.")
-        return v
+        return validate_password_complexity(v)
 
 
 class UserResponseSchema(UserBase):
@@ -47,8 +49,14 @@ class PasswordResetRequestSchema(UserBase):
 
 
 class PasswordResetCompleteSchema(UserBase):
+    email: EmailStr
     token: str
     password: str
+
+    @field_validator("password")
+    @classmethod
+    def validate_password(cls, v: str) -> str:
+        return validate_password_complexity(v)
 
 
 class UserLoginResponseSchema(BaseModel):
